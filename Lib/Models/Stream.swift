c691cfadd58d3cd80a4f7f9712b040aacb7e6fc1
Eric <epv9@case.edu>
2021-06-09 19:26:14 -0700

    added unit tests

--- Lib/Models/Stream.swift
+++ Lib/Models/Stream.swift
@@ -7,13 +7,14 @@
 
 import Foundation
 
+/// Type specific to `StreamResponse`  see https://dev.twitch.tv/docs/api/reference/#get-followed-streams
 public enum StreamType: String, Codable {
     case live
     case vodcast
     case none = ""
     case all
 }
-
+/// Response object for stream request: https://dev.twitch.tv/docs/api/reference/#get-followed-streams
 public struct StreamResponse: Codable, Equatable {
     public let streams: [Stream]
     public let pagination: Paginate?
@@ -24,10 +25,13 @@ public struct StreamResponse: Codable, Equatable {
     }
 }
 
+/// Pagination object used for certain API responses
 public struct Paginate: Codable, Equatable {
+    /// `string` representing your pagination cursor
     public let cursor: String?
 }
 
+/// underlying stream object see `StreamResponse`  and https://dev.twitch.tv/docs/api/reference/#get-followed-streams
 public struct Stream: Codable, Equatable {
     public let id: String
     public let userId: String
